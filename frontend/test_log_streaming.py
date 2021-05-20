467b72cf92bf0b8677c8633ccfaed9bca2f35f46
Ritik Mishra <ritik.mishra314@gmail.com>
2021-05-20 08:57:39 -0500

    test another way to stream logs

--- frontend/test_log_streaming.py
+++ frontend/test_log_streaming.py
@@ -1,7 +1,8 @@
 import asyncio
 import io
 import logging
-from typing import AsyncGenerator
+import logging.handlers
+from typing import Any, AsyncGenerator, cast
 
 from fastapi.responses import PlainTextResponse, StreamingResponse
 from fastapi.routing import APIRouter
@@ -10,14 +11,14 @@ from fastapi.routing import APIRouter
 async def some_random_task() -> None:
     logger = logging.getLogger("some random task")
     for i in range(10):
-        await asyncio.sleep(0.6)
+        await asyncio.sleep(0.7)
         logger.info(f"doing some work! {i}")
 
 
 router = APIRouter(prefix="/logstreamtest")
 
 
-async def periodic_read(
+async def stream_logs_polling(
     task: asyncio.Task, period: float = 0.5
 ) -> AsyncGenerator[str, None]:
     """This generator yields the content that was added to stdout as long as some task is alive.
@@ -52,8 +53,40 @@ async def periodic_read(
         last_read_pos += len(to_yield)
 
 
+async def stream_logs_async(
+    task: asyncio.Task, period: float = 0.5
+) -> AsyncGenerator[str, None]:
+    """This generator yields the content that was added to stdout as long as some task is alive.
+    This adds a logging handler that adds new log messages to a queue. Each time a new message arrives
+    in the queue, it is sent to the client.
+
+    Args:
+        task (asyncio.Task): Some task that is emitting stuff to sys.stdout.
+        period (float, optional): How often (in seconds) to check that new things have been printed. Defaults to 0.5.
+
+    Yields:
+        str: new content added to stdout
+    """
+    root_logger = logging.getLogger()
+
+    logs: "asyncio.Queue[logging.LogRecord]" = asyncio.Queue()
+    ch = logging.handlers.QueueHandler(cast(Any, logs))
+    ch.setLevel(logging.DEBUG)
+
+    formatter = logging.Formatter(
+        "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+    )
+    ch.setFormatter(formatter)
+
+    root_logger.addHandler(ch)
+
+    while not task.done():
+        log_record = await logs.get()
+        yield log_record.msg + "\n"
+
+
 @router.get("/")
-async def stream_logs() -> StreamingResponse:
+async def stream_logs_example() -> StreamingResponse:
     return StreamingResponse(
-        periodic_read(asyncio.create_task(some_random_task()), 1.0)
+        stream_logs_async(asyncio.create_task(some_random_task()), 1.0)
     )
