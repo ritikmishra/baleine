3dd8ecf15c6699ec94a95dd248badb5f8e8bb646
Ritik Mishra <ritik.mishra314@gmail.com>
2021-08-07 01:36:55 -0500

    split rally into rally and merge fleets

--- scripts/tasks/rally.py
+++ scripts/tasks/rally.py
@@ -13,7 +13,7 @@ async def rally_ships_to_world_id(
     ship_resource_id: int,
     ship_qty: Optional[int],
     destination_world_id: int,
-) -> None:
+) -> List[int]:
     """Rally ships to a particular world ID
 
     Args:
@@ -24,14 +24,17 @@ async def rally_ships_to_world_id(
             send as many ships as possible from across the empire to these worlds
         destination_world_id (int): World ID to send
 
+    Returns:
+        List[int]: A list of all of the fleet IDs that were created
+
     Raises:
         ValueError: [description]
     """
     logger = logging.getLogger("rally ships")
 
-    amount_of_resource_on_world: Callable[
-        [World], float
-    ] = lambda w: w.resource_dict[ship_resource_id]
+    amount_of_resource_on_world: Callable[[World], float] = lambda w: w.resource_dict[
+        ship_resource_id
+    ]
 
     # List of worlds with the ship sorted by most to least
     worlds_with_resource = sorted(
@@ -44,20 +47,21 @@ async def rally_ships_to_world_id(
     )
 
     # Check that we have enough ships across the empire to rally the desired amount
-    total_amount_of_resource = sum(map(amount_of_resource_on_world, worlds_with_resource))
-    if (
-        ship_qty is not None
-        and ship_qty > total_amount_of_resource
-    ):
+    total_amount_of_resource = sum(
+        map(amount_of_resource_on_world, worlds_with_resource)
+    )
+    if ship_qty is not None and ship_qty > total_amount_of_resource:
         raise ValueError("not enough ships to rally!")
 
-    qty_left_to_send = ship_qty if ship_qty is not None else int(total_amount_of_resource)
+    qty_left_to_send = (
+        ship_qty if ship_qty is not None else int(total_amount_of_resource)
+    )
     fleets: List[Fleet] = []
     for world in worlds_with_resource:
         qty_to_deploy = int(min(amount_of_resource_on_world(world), qty_left_to_send))
         if qty_to_deploy < 1:
             break
- 
+
         logger.info(
             f"Deploying {qty_to_deploy} ships from planet {world.name} (id {world.id})"
         )
@@ -67,30 +71,31 @@ async def rally_ships_to_world_id(
 
         qty_left_to_send -= qty_to_deploy
 
-    fleet_waiting_tasks: List[asyncio.Task[Fleet]] = []
-
     for i, fleet in enumerate(fleets):
-
         logger.info(
             f"({i+1}/{len(fleets)}) sending fleet to world id {destination_world_id}"
         )
         await context.set_fleet_destination(fleet.id, destination_world_id)
-        fleet_waiting_tasks.append(
-            asyncio.create_task(
-                fleet_manipulation_utils.wait_for_fleet(context, fleet.id)
-            )
-        )
 
-    fleets = await asyncio.gather(*fleet_waiting_tasks)
+    return [fleet.id for fleet in fleets]
+    # done!
 
-    master_fleet, *fleets_to_merge = fleets
+
+async def merge_fleets_in_transit(context: Anacreon, fleet_ids: List[int]) -> None:
+    logger = logging.getLogger("merge fleets in transit")
+    fleet_waiting_tasks = [
+        asyncio.create_task(fleet_manipulation_utils.wait_for_fleet(context, fleet_id))
+        for fleet_id in fleet_ids
+    ]
+    await asyncio.gather(*fleet_waiting_tasks)
+
+    master_fleet, *fleets_to_merge = fleet_ids
 
     logger.info(
-        f"fleets arrived, merging fleets into fleet {master_fleet.name} (id {master_fleet.id})"
+        f"fleets arrived, merging fleets into fleet id {master_fleet}"
     )
 
     for fleet in fleets_to_merge:
-        await context.transfer_fleet(master_fleet.id, fleet.id, fleet.resources)
-
-    # done!
-
+        fleet_resources = context.space_objects[fleet].resources
+        assert fleet_resources is not None
+        await context.transfer_fleet(master_fleet, fleet, fleet_resources)
