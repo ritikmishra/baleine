b4ab1adcb96f01b8cef7794251a38b3f43ec6c47
Ritik Mishra <ritik.mishra314@gmail.com>
2021-08-07 01:37:07 -0500

    aff some utility filters

--- scripts/filters.py
+++ scripts/filters.py
@@ -3,7 +3,7 @@ from anacreonlib.anacreon import Anacreon
 
 from anacreonlib.types.response_datatypes import World
 
-from scripts import utils
+from anacreonlib import utils
 from scripts.tasks import NameOrId
 
 
@@ -13,9 +13,20 @@ def dist_filter(
     if isinstance(center_planet, int):
         world = context.space_objects[center_planet]
     else:
-        world = next(w for w in context.space_objects.values() if w.name == center_planet)
+        world = next(
+            w for w in context.space_objects.values() if w.name == center_planet
+        )
 
     def filter_planet(other_world: World) -> bool:
         return utils.dist(world.pos, other_world.pos) <= radius
 
     return filter_planet
+
+
+def world_is_not_high_tech_trace_tril(context: Anacreon, world: World) -> bool:
+    return world.tech_level < 9 or not utils.world_has_trait(
+        context.game_info.scenario_info, world, context.game_info.find_by_unid("core.trillumRare").id
+    )
+
+def world_sf_below(context: Anacreon, sf_threshold: float, world: World) -> bool:
+    return (world.resources is not None) and context.calculate_forces(world).space_forces <= sf_threshold
